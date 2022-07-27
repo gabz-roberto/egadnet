64f38f59d29d799cf0f6db9bdc2103e4a809eedb
Gabriel Roberto <gabrielrobertoal@gmail.com>
2022-07-27 10:46:36 -0300

    front final

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,14 +1,48 @@
 import React, { useState } from "react";
-import axios from "axios";
+import Title from "../Title/Title";
 
-import { baseURL } from "../../../../server/config/api";
+import instance from "../../api/api";
 
 import "./Form.css";
 
 const Form = () => {
+  const [street, setStreet] = useState("");
+  const [district, setDistrict] = useState("");
+  const [city, setCity] = useState("");
+  const [uf, setUf] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    const formatedData = data.cep.replace(/\D/g, "");
+
+    if (formatedData.length !== 8) {
+      alert("CEP Inválido");
+      return;
+    } else {
+      fetch(`https://viacep.com.br/ws/${formatedData}/json/`)
+        .then((resp) => resp.json())
+        .then((obj) => {
+          setStreet(obj.logradouro || 'Rua não especificada');
+          setDistrict(obj.bairro || 'Bairro não especificado');
+          setCity(obj.localidade || 'Cidade não especificada');
+          setUf(obj.uf || 'UF não especificada');
+        });
+    }
+  };
+
   return (
-    <form className="form">
-      <input type="text" placeholder="CEP" />
+    <form className="form" onSubmit={handleSubmit}>
+      <Title title="Buscar CEP" />
+      <input type="text" placeholder="CEP" name="cep" />
+      <input type="text" placeholder="Rua" readOnly value={street} />
+      <input type="text" placeholder="Bairro" readOnly value={district} />
+      <input type="text" placeholder="Cidade" readOnly value={city} />
+      <input type="text" placeholder="Estado" readOnly value={uf} />
+
+      <button className="button">Buscar</button>
     </form>
   );
 };
--- server/config/api.js
+++
@@ -1,3 +0,0 @@
-export const baseURL = (cep) => {
-    return `https://viacep.com.br/ws/${cep}/json/`
-}
